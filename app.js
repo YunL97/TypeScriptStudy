784ecc7153c290a97c4d61de4e4970d9ff22860a
YunL97 <leeys9932@naver.com>
2023-02-20 00:38:49 +0900

    타입스크립트강의 19까지

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('asdf');
+function add(n1, n2, ac) {
+    if (ac) {
+        console.log('asdfff');
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var cc = add(1, 2, true);
+console.log('asdf' + cc);
